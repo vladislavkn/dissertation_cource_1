374cfec52f7d7203bacef8f06421fc4152c9b9ff
vladislavkn <inova136@gmail.com>
2022-05-26 13:21:18 +0300

    Initial Commit

---
+++ dissertation_cource_1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  dissertation_cource_1
+//
+//  Created by admin on 26.05.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
